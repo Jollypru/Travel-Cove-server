a091e0f0562e0c2106a6bb6614dc6e8c0bc6d330
Jollypru <Jollyprumarma2020@gmail.com>
2025-01-17 02:34:51 +0600

    user related api added

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const multer = require('multer');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+// const upload = multer({ dest: 'uploads/' });
 
 app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nor5r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,6 +27,43 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const userCollection = client.db('TourismDB').collection('users');
+    const storiesCollection = client.db('TourismDB').collection('stories');
+    const packageCollection = client.db('TourismDB').collection('packages');
+
+    // user related API
+
+    app.get('/users', async(req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.post('/users', async(req, res) => {
+      const user = req.body;
+      const query = {email: user.email};
+      const existingUser = await userCollection.findOne(query);
+      if(existingUser){
+        return res.send({message: 'user already exist', insertedId: null})
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+    // package related api
+    app.get('/packages', async(req, res) => {
+      const result = await packageCollection.find().toArray();
+      res.send(result);
+    })
+
+    // stories related api
+    app.get('/stories', async(req, res) => {
+        const stories = await storiesCollection.find().toArray();
+        res.send(stories);
+    })
+
+    
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
