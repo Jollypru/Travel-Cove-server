7efe704010d8fdd682f02e757a99f5d3dea566ed
Jollypru <Jollyprumarma2020@gmail.com>
2025-01-23 13:34:40 +0600

    random 4 story get api

--- index.js
+++ index.js
@@ -46,101 +46,84 @@ async function run() {
     const guideApplicationCollection = client.db('TourismDB').collection('guideApplications')
     const bookingsCollection = client.db('TourismDB').collection('bookings')
 
-    app.post('/jwt', async(req, res) => {
+    app.post('/jwt', async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '5h'});
-      res.send({token});
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5h' });
+      res.send({ token });
     })
 
     // middlewares
     const verifyToken = (req, res, next) => {
       // console.log('inside verify token',req.headers.authorization);
-      if(!req.headers.authorization){
-        return res.status(401).send({message: 'funauthorized access'});
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
       }
       const token = req.headers.authorization.split(' ')[1];
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
-        if(error){
-          return res.status(401).send({message: 'unauthorized access'})
+        if (error) {
+          return res.status(401).send({ message: 'unauthorized access' })
         }
         req.decoded = decoded;
+        console.log(req.decoded);
         next();
-      })     
+      })
     }
 
-    const verifyAdmin = async(req, res, next) =>{
+    const verifyAdmin = async (req, res, next) => {
       const email = req.decoded.email;
-      const query = {email: email};
+      const query = { email: email };
       const user = await userCollection.findOne(query);
       const isAdmin = user?.role === 'admin';
-      if(!isAdmin){
-        return res.status(403).send({message: 'forbidden access'})
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' })
       }
       next();
     }
 
     // user related API
 
-    // app.get('/users', async (req, res) => {
-    //   const email = req.query.email;
-    //   try {
-    //     if (email) {
-    //       const user = await userCollection.findOne({ email });
-    //       if (!user) {
-    //         return res.send({ message: 'User not found' })
-    //       }
-    //       return res.send(user);
-    //     }
-    //     else {
-    //       const users = await userCollection.find().toArray();
-    //       return res.send(users);
-    //     }
-    //   } catch (error) {
-    //     console.log('Error fetching users:', error);
-    //     res.send({ message: 'failed to fetch users' })
-    //   }
-
-    // })
-
-    app.get('/users',verifyToken,verifyAdmin, async (req, res) => {
+    app.get('/users', async (req, res) => {
       const { name, email, role } = req.query;
-
+  
       try {
-        const query = {};
-
-        if (name) {
-          query.name = { $regex: name, $options: 'i' };  // Case-insensitive search for name
-        }
-        if (email) {
-          query.email = { $regex: email, $options: 'i' }; // Case-insensitive search for email
-        }
-        if (role) {
-          query.role = role;
-        }
-
-        const users = await userCollection.find(query).toArray();
-        res.send(users);
+          const query = {};
+          if (email) {
+              query.email = { $regex: email, $options: 'i' };
+          }
+  
+          if (name) {
+              query.name = { $regex: name, $options: 'i' }; 
+          }
+          if (role) {
+              query.role = role;
+          }
+  
+          const users = await userCollection.find(query).toArray();
+          res.send(users.length === 1 ? users[0] : users); 
       } catch (error) {
-        console.log('Error fetching users:', error);
-        res.status(500).send({ message: 'Failed to fetch users' });
+          console.error('Error fetching users:', error);
+          res.status(500).send({ message: 'Failed to fetch users' });
       }
-    });
+  });
+  
 
-    app.get('/users/admin/:email', verifyToken, async(req, res) =>{
+    // for admin verification
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
       const email = req.params.email;
-      if(email !== req.decoded.email){
-        return res.status(403).send({message: 'forbidden access'})
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
       }
-      const query = {email: email};
+      const query = { email: email };
       const user = await userCollection.findOne(query);
       let admin = false;
-      if(user){
+      if (user) {
         admin = user?.role === 'admin'
       }
-      res.send({admin});
+      res.send({ admin });
     })
 
-    app.patch('/users/admin/:id',verifyToken, verifyAdmin, async (req, res) => {
+    // made a specific user admin
+    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
@@ -152,23 +135,44 @@ async function run() {
       res.send(result);
     })
 
-
+    // new user register
     app.post('/users', async (req, res) => {
-      const user = req.body;
-      const query = { email: user.email };
+      const { name, email, photo, role } = req.body;
+      const query = { email: email };
       const existingUser = await userCollection.findOne(query);
       if (existingUser) {
         return res.send({ message: 'user already exist', insertedId: null })
       }
-      const addUser = { name:user.name, email:user.email, role: 'tourist' }
+      const addUser = { name, email, photo, role: role || 'tourist', createdAt: new Date() }
       const result = await userCollection.insertOne(addUser);
       res.send(result);
     })
+
+    app.get('/admin/stats', async (req, res) => {
+      const totalPayment = await bookingsCollection.aggregate([
+        { $group: { _id: null, total: { $sum: '$amount' } } },
+      ]).toArray();
+
+      const totalTourGuides = await userCollection.countDocuments({ role: 'tour-guide' });
+      const totalPackages = await packageCollection.countDocuments();
+      const totalClients = await userCollection.countDocuments({ role: 'tourist' });
+      const totalStories = await storiesCollection.countDocuments();
+
+      res.send({
+        totalPayment: totalPayment[0]?.total || 0,
+        totalTourGuides,
+        totalPackages,
+        totalClients,
+        totalStories,
+      });
+    });
 
     const isValidObjectId = (id) => {
       return ObjectId.isValid(id) && String(new ObjectId(id)) === id;
     };
 
+
+    // to get tour-guide list
     app.get('/users/:id', async (req, res) => {
       const id = req.params.id;
       if (!isValidObjectId(id)) {
@@ -183,25 +187,54 @@ async function run() {
       res.send(result);
     })
 
+
+    // manage profile of user
+    app.patch('/users/profile/:id',verifyToken, async (req, res) => {
+      const { id } = req.params;
+      const { name, photo } = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          name: name, photo: photo, updatedAt: new Date()
+        }
+      }
+      const result = await userCollection.findOne(filter, updatedDoc);
+      if (result.matchedCount === 0) {
+        return res.status(404).send({ message: 'User not found' });
+      }
+      res.send({ message: 'Profile updated successfully.' });
+    })
+
     // package related api
     app.get('/packages', async (req, res) => {
       const result = await packageCollection.find().toArray();
       res.send(result);
     })
 
-    app.get('/packages/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      // console.log(query);
-      const result = await packageCollection.findOne(query);
-      res.send(result);
-    })
+    // app.get('/packages/:id', async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) };
+    //   // console.log(query);
+    //   const result = await packageCollection.findOne(query);
+    //   res.send(result);
+    // })
+
+    app.get('/packages/random', async (req, res) => {
+      try {
+          const randomPackages = await packageCollection.aggregate([{ $sample: { size: 3 } }]).toArray();
+          res.send(randomPackages);
+      } catch (error) {
+          console.error('Error fetching random packages:', error);
+          res.status(500).send({ message: 'Failed to fetch random packages' });
+      }
+  });
+  
 
     app.post('/packages', upload.fields([
-      {name: 'coverImage', maxCount: 1},
-      {name: 'galleryImages', maxCount: 10}
-    ]), async(req, res) => {
-      const {title, description,price, tourPlan, tourType} = req.body;
+      { name: 'coverImage', maxCount: 1 },
+      { name: 'galleryImages', maxCount: 10 }
+    ]), async (req, res) => {
+      const { title, description, price, tourPlan, tourType } = req.body;
       const coverImage = req.files?.coverImage?.[0]?.path;
       const galleryImages = req.files?.galleryImages?.map(file => file.path);
 
@@ -209,7 +242,7 @@ async function run() {
         title, description, price: parseFloat(price), tourPlan: JSON.parse(tourPlan), tourType, coverImage, galleryImages, createdAt: new Date()
       }
       const result = await packageCollection.insertOne(newPackage);
-      res.send({message: 'Package added successfully', packageId: result.insertedId })
+      res.send({ message: 'Package added successfully', packageId: result.insertedId })
     })
 
 
@@ -220,6 +253,19 @@ async function run() {
       const guides = await userCollection.find(query).toArray();
       res.send(guides);
     })
+
+    app.get('/guides/random', async (req, res) => {
+      try {
+          const randomGuides = await userCollection.aggregate([
+              { $match: { role: 'tour-guide' } }, // Ensure only tour guides are included
+              { $sample: { size: 6 } }
+          ]).toArray();
+          res.send(randomGuides);
+      } catch (error) {
+          console.error('Error fetching random tour guides:', error);
+          res.status(500).send({ message: 'Failed to fetch random tour guides' });
+      }
+  });
 
     // guide application related APIs
 
@@ -241,7 +287,7 @@ async function run() {
       // }
 
       const applicationData = {
-         name, email, title, reason, cvLink, status: 'pending', appliedAt: new Date()
+        name, email, title, reason, cvLink, status: 'pending', appliedAt: new Date()
       }
 
       const result = await guideApplicationCollection.insertOne(applicationData);
@@ -285,9 +331,12 @@ async function run() {
 
     // stories related api
     app.get('/stories', async (req, res) => {
-      const { userId } = req.query;
+      const { email } = req.query;
       try {
-        const query = userId ? { userId: new ObjectId(userId) } : {};
+        let query = {};
+        if(email){
+          query = {email: email}
+        }
         const stories = await storiesCollection.find(query).toArray();
         res.send(stories);
       } catch (error) {
@@ -295,6 +344,17 @@ async function run() {
         res.status(500).send({ message: 'Failed to fetch stories' });
       }
     });
+
+    app.get('/stories/random', async (req, res) => {
+      try {
+          const stories = await storiesCollection.aggregate([{ $sample: { size: 4 } }]).toArray();
+          res.send(stories);
+      } catch (error) {
+          console.error('Error fetching random stories:', error);
+          res.status(500).send({ message: 'Failed to fetch random stories' });
+      }
+  });
+
     app.post('/stories', upload.array('images', 5), async (req, res) => {
       try {
         const { title, description, userId } = req.body;
